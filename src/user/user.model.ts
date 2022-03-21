cf7b4381932c4f0329cb94bb7362bd77929348e1
sgu6ena <issar@admin.idknet.com>
2022-03-21 17:54:03 +0200

    mongo init auth init user init

---
+++ src/user/user.model.ts
@@ -0,0 +1,19 @@
+import {Base, TimeStamps} from "@typegoose/typegoose/lib/defaultClasses";
+import {prop} from "@typegoose/typegoose";
+
+export interface UserModel extends Base {
+}
+
+export class UserModel extends TimeStamps {
+    @prop({unique: true})
+    email: string;
+
+    @prop()
+    password: string;
+
+    @prop({default:false})
+    isAdmin: boolean;
+
+    @prop({default:[]})
+    favorites?: [];
+}
