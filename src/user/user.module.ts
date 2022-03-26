93b3472c2558df5cc8218a26f4a8b128b1a3b5b9
sgu6ena <sgu6ena@gmail.com>
2022-03-26 15:00:24 +0200

    add guard

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import {Module} from '@nestjs/common';
+import {UserService} from './user.service';
+import {UserController} from './user.controller';
+import {TypegooseModule} from "nestjs-typegoose";
+import {UserModel} from "./user.model";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  controllers: [UserController],
-  providers: [UserService],
+    imports: [
+        TypegooseModule.forFeature([
+            {
+                typegooseClass: UserModel,
+                schemaOptions: {
+                    collection: 'User'
+                },
+            },
+        ]),
+        ConfigModule,
+    ],
+    controllers: [UserController],
+    providers: [UserService],
 
 })
-export class UserModule {}
+export class UserModule {
+}
