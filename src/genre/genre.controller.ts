8c112da378b95c3355a40174d0a6f27668735cca
sgu6ena <sgu6ena@gmail.com>
2022-03-27 21:41:57 +0300

    genres

---
+++ src/genre/genre.controller.ts
@@ -0,0 +1,60 @@
+import {Body, Controller, Delete, Get, HttpCode, Param, Put, Query, UsePipes,Post, ValidationPipe} from '@nestjs/common';
+import {GenreService} from "./genre.service";
+import {Auth} from "../auth/decorators/auth.decorator";
+import {CreateGenreDto} from "./dto/create-genre.dto"
+import {IdValidationPipe} from "../pipes/id.validation.pipe";
+
+@Controller('genres')
+export class GenreController {
+    constructor(private readonly genreService: GenreService) {
+    }
+
+    @Get('by-slug/:slug')
+    async getProfile(@Param("slug") slug: string) {
+        return this.genreService.bySlug(slug)
+    }
+
+    @Get('/collections')
+    async getCollections() {
+        return this.genreService.getCollections()
+    }
+
+    @Get()
+    async getAll(@Query('searchTerm') searchTerm?: string) {
+        return this.genreService.getAll(searchTerm)
+    }
+
+    @Get(':id')
+    @Auth('admin')
+    async get(@Param('id', IdValidationPipe) id: string) {
+        return this.genreService.byId(id)
+    }
+
+    @UsePipes(new ValidationPipe())
+    @Post()
+    @HttpCode(200)
+    @Auth('admin')
+    async create() {
+        return this.genreService.create()
+    }
+
+    @UsePipes(new ValidationPipe())
+    @Put(':id')
+    @HttpCode(200)
+    @Auth('admin')
+    async update(
+        @Param('id', IdValidationPipe) id: string,
+        @Body() dto: CreateGenreDto
+    ) {
+        return this.genreService.update(id, dto)
+    }
+
+    @Delete(':id')
+    @HttpCode(200)
+    @Auth('admin')
+    async deleteGenre(
+        @Param('id', IdValidationPipe) id: string,
+    ) {
+        return this.genreService.delete(id)
+    }
+}
